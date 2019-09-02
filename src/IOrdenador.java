08c9ce0b5d15af35d445f7237ca1ea0222ccdd49
arthurpiratas <silva.arthursoares@gmail.com>
2019-09-01 21:31:48 -0300

    Implementação da Interface

--- src/IOrdenador.java
+++ src/IOrdenador.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 
 public interface IOrdenador {
-
+	
+	public void inserirColecao(ArrayList<String> pColecao);
+	public void informarValor(String pValor); 
+	public double getMaiorValor(); 
+	public double getMenorValor();
+	public double getValorMedio(); 
+	public double getValorMediana(); 
 }
--- src/Ordenador.java
+++ src/Ordenador.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
 
-public class Ordenador {
+public class Ordenador implements IOrdenador{
+
+	@Override
+	public void inserirColecao(ArrayList<String> pColecao) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void informarValor(String pValor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getMaiorValor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMenorValor() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getValorMedio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getValorMediana() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
